f87a2c86bc12af9191b12d926b6d0c7e134ae942
erikdao <cuong@neuraltalks.io>
2019-06-10 21:56:22 +0700

    Add: models for total financial costs and budget scenarios

--- cba/core/models.py
+++ cba/core/models.py
@@ -77,3 +77,111 @@ class CBAResult(models.Model):
 
     class Meta:
         verbose_name = _('cost benefit analysis result')
+
+
+class TotalFinancialCost(models.Model):
+    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
+    province = models.ForeignKey(
+        'administrations.AdminUnit', on_delete=models.SET_NULL, null=True
+    )
+
+    # Financial cost for 20 years
+    work_cost_y1 = models.FloatField(_('work cost year 1'), null=True, blank=True)
+    work_cost_y2 = models.FloatField(_('work cost year 2'), null=True, blank=True)
+    work_cost_y3 = models.FloatField(_('work cost year 3'), null=True, blank=True)
+    work_cost_y4 = models.FloatField(_('work cost year 4'), null=True, blank=True)
+    work_cost_y5 = models.FloatField(_('work cost year 5'), null=True, blank=True)
+    work_cost_y6 = models.FloatField(_('work cost year 6'), null=True, blank=True)
+    work_cost_y7 = models.FloatField(_('work cost year 7'), null=True, blank=True)
+    work_cost_y8 = models.FloatField(_('work cost year 8'), null=True, blank=True)
+    work_cost_y9 = models.FloatField(_('work cost year 9'), null=True, blank=True)
+    work_cost_y10 = models.FloatField(_('work cost year 10'), null=True, blank=True)
+    work_cost_y11 = models.FloatField(_('work cost year 11'), null=True, blank=True)
+    work_cost_y12 = models.FloatField(_('work cost year 12'), null=True, blank=True)
+    work_cost_y13 = models.FloatField(_('work cost year 13'), null=True, blank=True)
+    work_cost_y14 = models.FloatField(_('work cost year 14'), null=True, blank=True)
+    work_cost_y15 = models.FloatField(_('work cost year 15'), null=True, blank=True)
+    work_cost_y16 = models.FloatField(_('work cost year 16'), null=True, blank=True)
+    work_cost_y17 = models.FloatField(_('work cost year 17'), null=True, blank=True)
+    work_cost_y18 = models.FloatField(_('work cost year 18'), null=True, blank=True)
+    work_cost_y19 = models.FloatField(_('work cost year 19'), null=True, blank=True)
+    work_cost_y20 = models.FloatField(_('work cost year 20'), null=True, blank=True)
+    work_cost_total = models.FloatField(_('work cost total'), null=True, blank=True)
+
+    # npv for 20 years
+    npv_y1 = models.FloatField(_('npv year 1'), null=True, blank=True)
+    npv_y2 = models.FloatField(_('npv year 2'), null=True, blank=True)
+    npv_y3 = models.FloatField(_('npv year 3'), null=True, blank=True)
+    npv_y4 = models.FloatField(_('npv year 4'), null=True, blank=True)
+    npv_y5 = models.FloatField(_('npv year 5'), null=True, blank=True)
+    npv_y6 = models.FloatField(_('npv year 6'), null=True, blank=True)
+    npv_y7 = models.FloatField(_('npv year 7'), null=True, blank=True)
+    npv_y8 = models.FloatField(_('npv year 8'), null=True, blank=True)
+    npv_y9 = models.FloatField(_('npv year 9'), null=True, blank=True)
+    npv_y10 = models.FloatField(_('npv year 10'), null=True, blank=True)
+    npv_y11 = models.FloatField(_('npv year 11'), null=True, blank=True)
+    npv_y12 = models.FloatField(_('npv year 12'), null=True, blank=True)
+    npv_y13 = models.FloatField(_('npv year 13'), null=True, blank=True)
+    npv_y14 = models.FloatField(_('npv year 14'), null=True, blank=True)
+    npv_y15 = models.FloatField(_('npv year 15'), null=True, blank=True)
+    npv_y16 = models.FloatField(_('npv year 16'), null=True, blank=True)
+    npv_y17 = models.FloatField(_('npv year 17'), null=True, blank=True)
+    npv_y18 = models.FloatField(_('npv year 18'), null=True, blank=True)
+    npv_y19 = models.FloatField(_('npv year 19'), null=True, blank=True)
+    npv_y20 = models.FloatField(_('npv year 20'), null=True, blank=True)
+    npv_total = models.FloatField(_('npv total'), null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Total Financial Cost')
+
+
+class BudgetScenario(models.Model):
+    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
+
+    province = models.ForeignKey(
+        'administrations.AdminUnit', on_delete=models.SET_NULL, null=True
+    )
+
+    # 3 years rolling or 5 years rolling
+    program = models.SmallIntegerField(
+        _('rolling program'), null=True, blank=True, default=3)
+    
+    budget_percent = models.FloatField(
+        _('budget percentage'), null=True, blank=True, default=100.0
+    )
+    total_capital = models.FloatField(
+        _('total capital cost'), null=True, blank=True
+    )
+
+    periodic_maintenance_cost = models.FloatField(
+        _('period maintenance cost'), null=True, blank=True
+    )
+    rehabilitation_financial_cost = models.FloatField(
+        _('Rehabilitation Financial Cost'), null=True, blank=True
+    )
+    recurrent_cost = models.FloatField(
+        _('recurrent cost'), null=True, blank=True
+    )
+    total_agency_cost = models.FloatField(
+        _('Total Capital Cost'), null=True, blank=True
+    )
+    periodic_maintenance_road_works = models.FloatField(
+        _('Periodic Maintenance Road Works'), null=True, blank=True
+    )
+    rehabilitation_road_works = models.FloatField(
+        _('Rehabilitation Road Works'), null=True, blank=True
+    )
+    total_capital_road_works = models.FloatField(
+        _('Total Capital Road Works'), null=True, blank=True
+    )
+    total_recurrent_road_works = models.FloatField(
+        _('Total Capital Road Works'), null=True, blank=True
+    )
+
+    no_prioritised_sections = models.SmallIntegerField(
+        _('number of selected priority road sections'), null=True, blank=True
+    )
+    npv = models.FloatField(_('npv'), null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('budget scenario')
